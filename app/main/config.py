fbe0bf8496462637f3436ea6f99c5f9efe107ca1
Maciej Tarsa <44335423+maciejtarsa@users.noreply.github.com>
2021-03-30 15:32:02 +0100

    Update config.py
    included pathway to the MySQL db stored on AWS

--- app/main/config.py
+++ app/main/config.py
@@ -4,7 +4,10 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'super_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'main.db')
+    # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
+    SQLALCHEMY_DATABASE_URI = 'mysql://group17Admin:Group17Password@group17-githubjobs.cbgj5urfzqbw.us-east-1.rds.amazonaws.com:3306/JobSearchAPI'
+    # Uncomment the line below if you want to work with a local DB
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'main.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
 
