7faa59bf80b2033578f3fdabac7fd6dde5bacb47
Jyothsna P <jyothsnap97@gmail.com>
2021-04-05 16:18:53 +0100

    Update __init__.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -8,14 +8,15 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 from.config import Config
-flask_bcrypt = Bcrypt()
-db = SQLAlchemy()
+flask_bcrypt = Bcrypt() # for encryption of passwords
+db = SQLAlchemy() # adapter for any database. takes care of the operations of the database
 
 def create_app():
     """"Create app context"""
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(Config) # config.py has all env variables like secret key and db urls etc
+    # Initialise databse object
     db.init_app(app)
-    flask_bcrypt.init_app(app)
+    flask_bcrypt.init_app(app) # starting point for the app. bcrypt searches for the secret key for the app. secret key used for password hashing
 
     return app
